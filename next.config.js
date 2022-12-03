92980d8db885a0f2a7af439a1d220cacd1f88922
Fermin <josefermin.hernandez@adevinta.com>
2022-12-03 19:08:41 +0100

    initial setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.giphy.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
